90e8d671cc08b3473730f0cdb1b87e31e251ec5d
Samuel <samuelabreudev@gmail.com>
2025-06-26 03:07:48 -0300

    New Component (Product_Card)

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,20 +1,22 @@
 import "./Home.css";
 import Carousel1 from "../components/Carousel1";
 import Navbar from "../components/Navbar_home";
+import Product_Card from "../components/Product_Card";
 
 function Home() {
     return (
         <>
             <div id='Header'>
-                <img className="img-fluid" src="src\assets\LOGOMARCA.jpg" alt="Logo" />
+                <img className="img-fluid h-25 d-inline-block w-100" src="src\assets\LOGOMARCA.jpg" alt="Logo" />
             </div>
             <div>
                 <Navbar />
             </div>
             <main>
                 <Carousel1 />
+                <Product_Card />
             </main>
-            <footer></footer>
+            <footer>@Samuel.Dev</footer>
         </>
     )
 }
--- src/components/Navbar_home.jsx
+++ src/components/Navbar_home.jsx
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom";
 function Navbar(){
     return(
         <>
-        <nav className="navbar navbar-expand-lg bg-body-secondary">
+        <nav className="navbar navbar-expand-lg bg-black ">
                 <div className='container-fluid d-flex justify-content-center'>
-                    <div id="navbar" className="navbar-collapse">
-                        <ul className="nav-underline navbar-nav mt-0">
-                            <li className="nav-item"> <Link className="nav-link" to="/Campanha1">Campanha 1</Link></li>
-                            <li className="nav-item"> <Link className="nav-link" to="/Campanha2">Campanha 2</Link></li>
-                            <li className="nav-item"> <Link className="nav-link" to="/Campanha3">Campanha 3</Link></li>
-                            <li className="nav-item"> <Link className="nav-link" to="/Campanha4">Campanha 4</Link></li>
+                    <div id="navbar" className="navbar-collapse ">
+                        <ul className="nav-underline navbar-nav mt-0 ">
+                            <li className="nav-item"> <Link className="nav-link text-white" to="/Campanha1">Campanha 1</Link></li>
+                            <li className="nav-item"> <Link className="nav-link text-white" to="/Campanha2">Campanha 2</Link></li>
+                            <li className="nav-item"> <Link className="nav-link text-white" to="/Campanha3">Campanha 3</Link></li>
+                            <li className="nav-item"> <Link className="nav-link text-white" to="/Campanha4">Campanha 4</Link></li>
                         </ul>
                     </div>
                 </div>
